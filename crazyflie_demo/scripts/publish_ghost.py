ae1666ae94919f9c226ab8e145227fa238b2b000
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-06-12 10:37:29 -0700

    Added parameter for selecting the frequency

--- crazyflie_demo/scripts/publish_ghost.py
+++ crazyflie_demo/scripts/publish_ghost.py
@@ -6,6 +6,7 @@ import time
 import uav_trajectory
 from threading import Thread
 from crazyflie_demo.msg import Trajectory 
+from tf.transformations import euler_from_matrix
 
 # Trajectory Publisher
 ghost_pub = rospy.Publisher('ghost_trajectory', Trajectory, queue_size=10)
@@ -34,15 +35,21 @@ def rep_trajectory(trajectory, start_position, freq):
             msg.py = rep_trj.pos[1]
             msg.pz = rep_trj.pos[2]
 
-
             msg.vx = rep_trj.vel[0]
             msg.vy = rep_trj.vel[1]
             msg.vz = rep_trj.vel[2]
 
-
             msg.accx = rep_trj.acc[0]
             msg.accy = rep_trj.acc[1]
             msg.accz = rep_trj.acc[2]
+
+            # Conver the Rotation matrix to euler angles
+            R = rep_trj.R
+            (roll, pitch, yaw) = euler_from_matrix(R)
+
+            msg.r = roll
+            msg.p = pitch
+            msg.y = yaw
 
             # Pubblish the evaluated trajectory
             ghost_pub.publish(msg)
@@ -61,6 +68,9 @@ if __name__ == '__main__':
         trj_file = rospy.get_param(v)
     else:
         rospy.signal_shutdown("Trjectory file not found!")
+ 
+    
+    frequency = rospy.get_param('freq', 30.0);
 
     
     print("Loading trajectory from: ", trj_file)
@@ -69,5 +79,5 @@ if __name__ == '__main__':
     
     print("Trajecotry duration: ", traj.duration)
 
-    t = Thread(target=rep_trajectory, args=(traj,[0,0,0], 10)).start()
+    t = Thread(target=rep_trajectory, args=(traj,[0,0,0], frequency)).start()
 
