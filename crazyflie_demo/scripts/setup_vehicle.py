11006306e522041103db899186a8c1343a4e6c3a
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-06-23 17:15:13 -0700

    Added argument to the setup_vehicle script

--- crazyflie_demo/scripts/setup_vehicle.py
+++ crazyflie_demo/scripts/setup_vehicle.py
@@ -1,48 +1,74 @@
 #!/usr/bin/env python
 
-# Python script to start a crazyflie mission.
+# Python script to setup the crazyflie algorithms
 # 
-# The script expects the name of the file as a parameter. It is also possible
-# to specify the frequency of the thread publishing the position of the 
-# 'ghost', that is the simulation of the expected trajectory.
-# 
-# Precisely the script will:
-#     Load a trajectory from file
-#     Upload the trajectory on the crazyflie
-#     Ask the crazyflie to takeoff
-#     Send the command to start the mission(trajectory)
-#     Start a thread that simulate the trjectory execution
-#     Ask the crazyflie to land after the end of the mission
-# 
+# - Set the commander level
+# - Set the estimation algorithm
+# - Set the control algorithm
 # 
 
 import rospy
 import crazyflie
 import time
-import uav_trajectory
-from threading import Thread
-from crazyflie_demo.msg import Trajectory 
-from tf.transformations import euler_from_matrix
 
 
 if __name__ == '__main__':
     rospy.init_node('Setup_node')
 
-    print("Starting Vehicle Setup")
+    print("Vehicle Setup")
 
-    cf = crazyflie.Crazyflie("cf1", "/tf")
+    est = 2
+    ctr = 2
 
-    while (cf.getParam("commander/enHighLevel") != 1): 
-        cf.setParam("commander/enHighLevel", 1)
+    # Read the parameters
+    cf_id = rospy.get_param('~cf', 'cf1')
+    comm_lev = rospy.get_param('~comm_lev', 1);
+    estimator = rospy.get_param('~Estimator', 'EKF')
+    controller = rospy.get_param('~Controller', 'Mellinger')
+    req_reset = rospy.get_param('~ResEstimator', 0)
 
-    while (cf.getParam("stabilizer/estimator") != 3):
-        cf.setParam("stabilizer/estimator", 3) # 1)Complementary 2)EKF 3)USC
+    print("Selecting CF: ", cf_id)
+    print('Selecting Commander Level: ', comm_lev)
+    print("Selecting Estimator: ", estimator)
+    print("Selecting Controller: ", controller)
 
-    while (cf.getParam("stabilizer/controller") != 2):
-        cf.setParam("stabilizer/controller", 2) # 1)PID  2)Mellinger 
+    # Create CF object
+    cf = crazyflie.Crazyflie(cf_id, "/tf")
+
+
+    while (cf.getParam("commander/enHighLevel") != comm_lev):
+        cf.setParam("commander/enHighLevel", comm_lev)
+
+    # Map the estimator name to index
+    if (estimator == 'EKF'):
+        est = 2
+    if (estimator == 'CMP'):
+        est = 1
+    if (estimator == 'USC'):
+        est = 3
+    # Set the estimator
+    while (cf.getParam("stabilizer/estimator") != est):
+        cf.setParam("stabilizer/estimator", est) # 1)Complementary 2)EKF 3)USC
+
+    print("Correctly selected ", cf.getParam("stabilizer/estimator")," estimator")
+    time.sleep(1)
+
+    # If Kalman reset the estimator
+    if (estimator == 'EKF' and req_reset):
+        cf.setParam("kalman/resetEstimation", 1)
+        cf.setParam("kalman/resetEstimation", 0)
+
+    # Map the controller name to index
+    if (controller == 'PID'):
+        ctr = 1
+    if (controller == 'Mellinger'):
+        ctr = 2
+    # Set the controller
+    while (cf.getParam("stabilizer/controller") != ctr):
+        cf.setParam("stabilizer/controller", ctr) # 1)PID  2)Mellinger
+    print("Correctly selected ", cf.getParam("stabilizer/controller")," estimator")
     time.sleep(3)
 
-    cf.setParam("kalman/resetEstimation", 1)
-    cf.setParam("kalman/resetEstimation", 0)
+
 
     
